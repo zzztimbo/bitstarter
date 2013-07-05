710ccb556da4fa4867ad9b13ca27a100ce5a560a
Tim Chan <tim@chan.net>
2013-07-04 21:57:15 -0700

    reading from index.html as page output

--- web.js
+++ web.js
@@ -2,8 +2,16 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var data; 
+
+fs.readFileSync('index.html', function (err, data) {
+  if (err) throw err;
+  console.log(data);
+}
+
 app.get('/', function(request, response) {
   response.send('Hello World 2!');
+  response.send(data.toString());
 });
 
 var port = process.env.PORT || 5000;
